95908d46902db08133b083854b75b7ace47b27ad
KingKai69 <kai.horlacher@gmx.de>
2022-11-30 13:04:05 +0100

    Funktioniert wieder

--- TrainingsApp/training_app/other_training/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Athlete(models.Model):
-    Name = models.CharField(max_length=200)
-    Size = models.IntegerField()
-    Weight = models.IntegerField()
