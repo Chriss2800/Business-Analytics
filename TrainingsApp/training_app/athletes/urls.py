ae5edeb82fc4d61172661ae0a828f79de4b8f649
Christoph Mattmann <christoph.mattmann@studmail.htw-aalen.de>
2022-12-07 08:09:15 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Chriss2800/Business-Analytics""
    This reverts commit f51fb8adce324796d913b647be1d108835e1b7a5.

---
+++ TrainingsApp/training_app/athletes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create_athlete/', views.create_athlete, name='create_athlete'),
+    path('create_course/', views.create_course, name='create_course'),
+    path('create_training_data/', views.create_training_data, name='create_training_data'),
+    
+]
