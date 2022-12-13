e59a22c7e21f1f18c7e3e916d951ff07b094c4d7
Christoph Mattmann <christoph.mattmann@studmail.htw-aalen.de>
2022-12-13 07:58:22 +0100

    R_Datei.R kommentiert
    Erste obligatorische Kommentare zur Verständnis des Codes.
    
    #StartedFromTheBottom

--- TrainingsApp/training_app/R_Datei.R
+++ TrainingsApp/training_app/R_Datei.R
@@ -3,7 +3,7 @@ library(dbplyr)
 library(RSQLite)
 
 
-file.exists("db.sqlite3")
-verbindung <- DBI::dbConnect(RSQLite::SQLite(), "db.sqlite3")
-athleten_tabelle <- tbl(verbindung, "athletes_athletes")
-head(athleten_tabelle, n = 10)
+file.exists("db.sqlite3") #zum testen ob die File überhaupt vorhanden ist. Kann später raus!
+verbindung <- DBI::dbConnect(RSQLite::SQLite(), "db.sqlite3") #Definition der COnnection von der SQLitedatei "db.sqlite3" mit der Variable "verbindung"
+athleten_tabelle <- tbl(verbindung, "athletes_athletes") #Aus der Verbindung mit "db.sqlite3" wird die Tabelle "athletes_athletes" in die variable "athleten_tabele" gespeichert
+head(athleten_tabelle, n = 10) # Zeigt die ersten 10 Einträge der "athleten_tabelle" Tabelle an. Dient zu Veranschauung, kann später raus.
