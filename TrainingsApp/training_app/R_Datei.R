55d0f0249d2a7f2a18ef7641854164a9d7f5e993
CorneliusFichtner <corneliusfichtner@web.de>
2022-12-30 12:11:53 +0100

    R all workouts of a specific athlete

--- TrainingsApp/training_app/R_Datei.R
+++ TrainingsApp/training_app/R_Datei.R
@@ -4,20 +4,43 @@ library(RSQLite)
 
 
 file.exists("db.sqlite3") #zum testen ob die File überhaupt vorhanden ist. Kann später raus!
-verbindung <- DBI::dbConnect(RSQLite::SQLite(), "db.sqlite3") #Definition der COnnection von der SQLitedatei "db.sqlite3" mit der Variable "verbindung"
+conn <- DBI::dbConnect(RSQLite::SQLite(), "db.sqlite3") #Definition der COnnection von der SQLitedatei "db.sqlite3" mit der Variable "verbindung
 
-athleten_tabelle <- tbl(verbindung, "athletes_athletes") #Aus der Verbindung mit "db.sqlite3" wird die Tabelle "athletes_athletes" in die variable "athleten_tabele" gespeichert
-workoutdata<- tbl(verbindung,"athletes_workout_data") #Workout data aus der SQLite extrahieren
-workoutdescription<- tbl(verbindung,"athletes_workout") #Workouts aus der SQLite extrahieren
+# Write the dataset into a table 
+dbListTables(conn)
 
-df_workout_data<- data.frame(workoutdata) # Convert dataframe to list using data.frame()
-df_athletes_table<- data.frame(athleten_tabelle) # Convert dataframe to list using data.frame()
-df_workout_description<- data.frame(workoutdescription) # Convert dataframe to list using data.frame()
+# get query to all tables
+athletes_workout = dbGetQuery(conn, "SELECT * FROM athletes_workout")
+athletes_athletes = dbGetQuery(conn, "SELECT * FROM athletes_athletes")
+athletes_workout_data = dbGetQuery(conn, "SELECT * FROM athletes_workout_data")
 
-head(athleten_tabelle, n = 10) # Zeigt die ersten 10 Einträge der "athleten_tabelle" Tabelle an. Dient zu Veranschauung, kann später raus.
-=======
-athlethes_counts<- df_athletes_table %>% #creating a output variable called athlethes_counts
-  group_by(id) %>% #group die id column to count the id
-  summarize(count=n()) #summarize to creat a count column with every id
-collect() #bring back the data 
-athlethes_counts #get back a athletes_count object
+#bring all data into one dataframe 
+#merge athlethes_workout and athletes_workout_data
+workout_and_workout_data = (right_join(athletes_workout, athletes_workout_data, by = c("id" ="workout_id")))
+#merge athletes_athletes and workout_and_workout_data into full_dataset
+full_dataset = (right_join(athletes_athletes, workout_and_workout_data, by = c("id" ="athlete_id")))
+#check if the dataset athletes_and_workout_data contains all variables
+#print(full_dataset)
+
+
+#all workouts of a specific athlete
+name = "Daniel"
+colums_for_all_workouts_specific_athlete = full_dataset[,c("first_name","last_name", "description","start","end","date")]
+all_workouts_specific_athlete = colums_for_all_workouts_specific_athlete[colums_for_all_workouts_specific_athlete$first_name == name,]
+print(all_workouts_specific_athlete)
+
+#average over training duration of a certain athlete over all trainings
+
+
+#result = left_join( dbGetQuery(conn, "SELECT * FROM athletes_workout_data") , dbGetQuery(conn, "SELECT * FROM athletes_athletes"), by = c("id"))
+
+
+
+#print(result)
+
+
+#get_duration_of_training <- function(start_time, end_time) {
+#  print(difftime(start_time, end_time, units = "mins"))
+#}
+
+
