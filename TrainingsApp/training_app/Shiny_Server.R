9da48e5e91562623fd1919c216ec6754dc0078da
Chriss280 <christoph.mattmann@studmail.htw-aalen.de>
2023-01-02 18:20:04 +0100

    Dashboard des Shiny Servers
    aktuell problematisch da die Adresse des Shiny Servers sich noch ändert

--- TrainingsApp/training_app/Shiny_Server.R
+++ TrainingsApp/training_app/Shiny_Server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 library(plotly)
+library(shinydashboardPlus)
+library(shinyWidgets)
 dashboardPage(
   dashboardHeader(),
   dashboardSidebar(),
@@ -8,7 +10,24 @@ dashboardPage(
 )
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Dashboard Athleten-Leistungen"),
+      dashboardHeader(title = "Result - Graphics",
+                  leftUi = tagList(
+                    actionButton(inputId = "home", label = "Home", status = "primary" , onclick ="location.href='http://127.0.0.1:8000/';",class="btn btn-primary btn btn-block"),
+                    actionButton(inputId = "athlet", label = "Create Athlete", status = "primary" , onclick ="location.href='http://127.0.0.1:8000/create_athlete/';",class="btn btn-primary btn btn-block"),
+                    actionButton(inputId = "training", label = "Create Training", status = "primary" , onclick ="location.href='http://127.0.0.1:8000/create_course/';",class="btn btn-primary btn btn-block"),
+                    dropdownButton(
+                      inputId = "mydropdown",
+                      label = "Training Data",
+                      status = "primary",
+                      circle = FALSE,
+                      actionButton(inputId = "c_workout", label = "Create Workout", onclick ="location.href='http://127.0.0.1:8000/create_training_data/';", class="btn btn-primary btn btn-block"),
+                      actionButton(inputId = "v_workout", label = "View Workout", onclick ="location.href='http://127.0.0.1:8000/workout_list';", class="btn btn-primary btn btn-block"),
+                      actionButton(inputId = "graphic", label = "Graphics", onclick ="location.href='http://127.0.0.1:8000/workout_list';", class="btn btn-primary btn btn-block")
+                                          ),
+                                                       )
+  ),
+                             
+                  
   dashboardSidebar(
     #Welchen Content es geben soll
     sidebarMenu(
@@ -18,7 +37,7 @@ ui <- dashboardPage(
   uiOutput("out1")
   ),
   dashboardBody(
-    #Inhalt der jeweiligen Seiten in den tewiligen Tabs unterteilt
+      #Inhalt der jeweiligen Seiten in den tewiligen Tabs unterteilt
     tabItems(
     tabItem(tabName = "Athlet",
             fluidRow(
