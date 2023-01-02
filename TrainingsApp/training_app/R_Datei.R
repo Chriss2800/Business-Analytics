7ffe93128c4138f4462ff4c4718623e431111bd3
CorneliusFichtner <corneliusfichtner@web.de>
2023-01-02 13:29:41 +0100

    R Task 4,5,6
    •Average of the training duration of a certain training over all athletes
    •Median of the training duration of a certain training over all athletes
    •Standard deviation of the training duration of a certain training over all athletes

--- TrainingsApp/training_app/R_Datei.R
+++ TrainingsApp/training_app/R_Datei.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(dbplyr)
 library(RSQLite)
+library(ggplot2)
 
 
 file.exists("db.sqlite3") #zum testen ob die File überhaupt vorhanden ist. Kann später raus!
@@ -46,6 +47,7 @@ for(i in 1:nrow(training_times)) {       # for-loop over rows
   counter=i
 }
 
+#Funktion um den Zeitunterschied zwischen zwei Zeiten zu berechnen
 get_duration_of_training <- function(start_time, end_time) {
   return(difftime(end_time, start_time, units = "mins"))
 }
@@ -54,7 +56,7 @@ average_training_duration= sum_training_duration/counter
 
 #print the sentence 
 #print(paste0("The average training duration of the athlet over all trainings is: ",toString(average_training_duration)))
-print(sprintf("The average training duration of the athlet over all trainings is: %s min",toString(average_training_duration)))
+print(sprintf("The average training duration of %s over all trainings is: %s min",name, toString(average_training_duration)))
 
 #AVERAGE OVER TRAINING DURATION OF A SPECIFIC TRAINING OF AN ATHLETE 
 
@@ -79,10 +81,70 @@ for(o in 1:nrow(training_times_specific_workout)) {       # for-loop over rows
 #print(sum_specific_training_duration)
 average_specific_training_duration= sum_specific_training_duration/counter_specific_training
 
-print(sprintf("The average training duration of a specific training of an athlete is: %s min",toString(average_specific_training_duration)))
+print(sprintf("The average training duration of %s of %s is: %s mins", description, name, toString(average_specific_training_duration)))
 
 #AVERAGE OF THE TRAINING DURATION OF A CERTAIN TRAINING OVER ALL ATHLETS
 
+description1 = "Joggen"
+certain_training_over_all_athletes = colums_for_all_workouts_specific_athlete[colums_for_all_workouts_specific_athlete$description == description1,]
+#check if the dataframe gives certain_training_over_all_athletes
+print(certain_training_over_all_athletes)
+
+training_times_certain_training = select(certain_training_over_all_athletes, date, start, end)
+
+sum_certain_training_duration = 0
+counter_certain_training=0
+
+for(u in 1:nrow(training_times_certain_training)) {       # for-loop over rows
+  start_time_certain_training = paste(toString(training_times_certain_training$date[u]), toString(training_times_certain_training$start[u]))
+  end_time_certain_training = paste(toString(training_times_certain_training$date[u]), toString(training_times_certain_training$end[u]))
+  sum_certain_training_duration = sum_certain_training_duration + get_duration_of_training(start_time = start_time_certain_training, end_time = end_time_certain_training)
+  counter_certain_training=u
+}
+
+#print(sum_certain_training_duration)
+average_certain_training_duration= sum_certain_training_duration/counter_certain_training
+
+print(sprintf("The average training duration of %s over all athletes is: %s mins", description1, toString(average_certain_training_duration)))
+
+
+# MEDIAN OF THE TRAINING DURATION OF A CERTAIN TRAINING OVER ALL ATHLETES
+list_certain_workout <- list()
+
+for (a in 1:nrow(training_times_certain_training)) {       # for-loop over rows
+  start_time_certain_training = paste(toString(training_times_certain_training$date[a]), toString(training_times_certain_training$start[a]))
+  end_time_certain_training = paste(toString(training_times_certain_training$date[a]), toString(training_times_certain_training$end[a]))
+  list_certain_workout [a] = get_duration_of_training(start_time = start_time_certain_training, end_time = end_time_certain_training)
+}
+
+#print(list_certain_workout)
+print(sprintf("The median of the training duration of %s over all athletes is: %s mins", description1, median(unlist(list_certain_workout))))
+
+#STANDARD DEVIATION OF THE TRAINING DURATION OF A CERTAIN TRAINING OVER ALL ATHLETES
+
+print(sprintf("The standard deviation of the training duration of %s over all athletes is: %s mins", description1, sd(unlist(list_certain_workout))))
+
+#DRAW APPROPRIATE DIAGRAMS TO VISUALIZE THE DATA USING GGPLOT
+#ALL TRAINING DURATION VALUES OF A SPECIFIC TRAINING
+description2 = "Joggen"
+specific_training_over_all_athletes = colums_for_all_workouts_specific_athlete[colums_for_all_workouts_specific_athlete$description == description2,]
+#check if the dataframe gives specific_training_over_all_athletes
+print(specific_training_over_all_athletes)
+
+training_times_specific_training = select(specific_training_over_all_athletes, date, start, end)
+
+list_specific_training <- list()
+
+for (a in 1:nrow(training_times_specific_training)) {       # for-loop over rows
+  start_time_certain_training = paste(toString(training_times_specific_training$date[a]), toString(training_times_specific_training$start[a]))
+  end_time_certain_training = paste(toString(training_times_specific_training$date[a]), toString(training_times_specific_training$end[a]))
+  list_specific_training [a] = get_duration_of_training(start_time = start_time_certain_training, end_time = end_time_certain_training)
+}
+print(list_specific_training)
+
+ggplot(list_specific_training,)
+
+#AVERAGE OF THE TRAINING DURATION OVER ALL ATHLETES (I.E. PER ATHLETE AVERAGE OF THE TRAINING DURATION OVER ALL TRAININGS)
 
 
 
@@ -91,19 +153,9 @@ print(sprintf("The average training duration of a specific training of an athlet
 
 
 
-
-
-
-
-#result = left_join( dbGetQuery(conn, "SELECT * FROM athletes_workout_data") , dbGetQuery(conn, "SELECT * FROM athletes_athletes"), by = c("id"))
-
-
-
-#print(result)
 
 
 #get_duration_of_training <- function(start_time, end_time) {
 #  print(difftime(start_time, end_time, units = "mins"))
 #}
-
 #dbDisconnect()
